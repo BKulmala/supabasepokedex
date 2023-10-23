9cad7b0643fc387af5316c55f2ab80bb10601bea
Brandon <bjk74@uakron.edu>
2023-10-23 11:45:14 -0400

    Initial RPC call to alter user table

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
 import { supabase } from '/src/api/hello.js'
 
-import {Button} from '@mui/material'
+import { Button } from '@mui/material'
 async function signInWithDiscord() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
+/*  const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'discord',
-  })
+  })*/
+  const { RPCData, RPCError } = await supabase.rpc('createuser')
+  .then(e => {console.log(e)});
 }
 
 function mainPage() {
